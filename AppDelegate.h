43952b67845b930943357b938523a2c7bf4e8b34
alexignatenko <auignatenko@gmail.com>
2018-01-30 06:47:06 +0300

    создание проекта

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  todoapp
+//
+//  Created by promocompany on 30.01.18.
+//  Copyright © 2018 Alex Ignatenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  todoapp
+//
+//  Created by promocompany on 30.01.18.
+//  Copyright © 2018 Alex Ignatenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
